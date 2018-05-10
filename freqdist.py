e356ea59854b0e817d511d2b3cc655a16fda5bb5
Thiago Curvelo <tcurvelo@gmail.com>
2018-05-10 13:08:38 -0300

    Remove stopwords

--- freqdist.py
+++ freqdist.py
@@ -1,17 +1,25 @@
-from nltk.probability import  FreqDist
+from nltk.probability import FreqDist
+from nltk.tokenize import word_tokenize, wordpunct_tokenize
+from nltk.corpus import stopwords
+
 from pprint import pprint
 
 import json
 import nltk
 
 nltk.download('punkt')
+nltk.download('stopwords')
 
+stop_words = set(stopwords.words('english'))
 
 with open('jobs.json') as jobsfile:
     jobs = json.loads(jobsfile.read())
 
 words = FreqDist([])
 for job in jobs:
-    words += FreqDist(nltk.tokenize.word_tokenize(job['text'].lower()))
+    new_words = [
+        w for w in wordpunct_tokenize('{} {}'.format(job['title'], job['text']).lower()) if w not in stop_words
+    ]
+    words += FreqDist(new_words)
 
 pprint(words.most_common(100))
